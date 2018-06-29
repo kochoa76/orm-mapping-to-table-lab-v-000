cb9732188bc3593abdb09618fe794e66e4e40f49
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-29 19:58:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,9 +2,9 @@ class Student
   attr_accessor :name, :grade
 
   def initialize(name, grade, id = nil)
-    @name = name 
+    @name = name
     @grade = grade
-    @id = id 
+    @id = id
   end 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]

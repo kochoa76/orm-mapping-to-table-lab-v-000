dbc23f4815bdabe31459e42f04399582c49a3a09
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-29 20:03:22 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
       students (id INTEGER PRIMARY KEY,
       name TEXT, grade INTEGER)
       SQL
-      DB[:conn](sql)
+      DB[:conn].execute(sql)
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   end
